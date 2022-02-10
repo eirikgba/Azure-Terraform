345f631bbff8f8b6140749c54c1479b92e08ad90
eirikgba@stud.ntnu.no <eirikgba@stud.ntnu.no>
2022-02-10 13:10:55 +0100

    Folder structure

---
+++ lvl6/variables.tf
@@ -0,0 +1,10 @@
+variable "web_resource_group_name" {
+    type    = string
+    default = "web_resource_group"
+}
+
+variable "compute_resource_group_name" {
+    type    = string
+    default = "compute_resource_group"
+}
+
